7dbf61019caed25fdbeb4fdd12021486e05d07af
Gok <154542453+Fahcos@users.noreply.github.com>
2024-05-07 07:11:17 +0000

    wow

--- ex02.c
+++ ex02.c
@@ -12,20 +12,20 @@ float d = 2.098765;
 double e = 2.123456789012345;
 double f = 2.098765432109876;
 
-    printf("Somme");
+    printf("Somme\n");
     printf("a+10 = %d\n", a + 10); //somma
-    printf("a+b = %lf\n", a+b); //somma tra variabili
+    printf("a+b = %d\n", a+b); //somma tra variabili
 
-    printf("Sottrazioni");
+    printf("Sottrazioni\n");
     printf("a-10 = %d\n", a-10); //differenza
-    printf("a-b = %lf\n", a-b); //differenza tra variabili
+    printf("c-d = %lf\n", c-d); //differenza tra variabili
 
-    printf("Moltiplicazioni");
+    printf("Moltiplicazioni\n");
     printf("a*10 = %d\n", a*10); //moltiplicazione
-    printf("a*b = %lf\n", a*b); //moltiplicazioni tra variabili
+    printf("a*f = %lf\n", a*f); //moltiplicazioni tra variabili
 
-    printf("Divisioni");
+    printf("Divisioni\n");
     printf("a/10 = %d\n", a/10); //divisione
-    printf("a/b = %lf\n", a/b); //divisione tra variabili
+    printf("e/f = %lf\n", e/f); //divisione tra variabili
 
 }
